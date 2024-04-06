6b73d43076126c5aba43bf842a61b584bea24b73
Marshal10 <marshalmaleane123@gmail.com>
2024-04-06 17:13:24 +0530

    Generate random quote

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+const quoteContainer=document.getElementById('quote-container')
+const quoteText=document.getElementById('quote')
+const authorText=document.getElementById('author')
+const xBtn=document.getElementById('x-btn')
+const newQuoteBtn=document.getElementById('new-quote-btn')
 let quotes=[]
+
+function newQuote(){
+    const newQuote=quotes[Math.floor(Math.random() * quotes.length)]
+    console.log(newQuote)
+}
+
 async function getQuotes(){
     const apiUrl='https://jacintodesign.github.io/quotes-api/data/quotes.json'
     try{
         const response=await fetch(apiUrl)
         quotes=await response.json()
+        newQuote()
     }catch(error){
 
     }
