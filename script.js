1dc580c72ce65edd2ca2fdacf9792b4e8e952cce
Marshal10 <marshalmaleane123@gmail.com>
2024-04-06 17:40:48 +0530

    Added a loader

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const xBtn = document.getElementById("x-btn");
 const newQuoteBtn = document.getElementById("new-quote-btn");
+const loader = document.querySelector(".loader");
 let quotes = [];
 
+function loading() {
+  quoteContainer.hidden = true;
+  loader.hidden = false;
+}
+
+function loadComplete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 function newQuote() {
+  loading();
   const newQuote = quotes[Math.floor(Math.random() * quotes.length)];
   console.log(newQuote);
   quoteText.textContent = newQuote.text;
@@ -16,9 +28,11 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote-text");
   }
+  loadComplete()
 }
 
 async function getQuotes() {
+  loading();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
@@ -27,12 +41,12 @@ async function getQuotes() {
   } catch (error) {}
 }
 
-function postOnX(){
-    const xUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
-    window.open(xUrl,'_blank')
+function postOnX() {
+  const xUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(xUrl, "_blank");
 }
 
-xBtn.addEventListener('click',postOnX)
-newQuoteBtn.addEventListener('click',newQuote)
+xBtn.addEventListener("click", postOnX);
+newQuoteBtn.addEventListener("click", newQuote);
 
 getQuotes();
